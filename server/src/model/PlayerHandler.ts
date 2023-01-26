096e950b91775271721824ff884ad79aed5de449
ArvidSve <73997840+ArvidSve@users.noreply.github.com>
2023-01-26 09:53:08 +0100

    Added code to PlayerHandler
    Added parameters, constructor and methods, addPlayer, removePlayer.

---
+++ server/src/model/PlayerHandler.ts
@@ -0,0 +1,19 @@
+export class PlayerHandler {
+
+    players : Array<Player>;
+    host : Player;
+
+
+    constructor(players : Array<Player>, host :_ Player) {
+        this.players = players;
+        this.host = host; 
+    }
+
+    addPlayer(player: Player) : void {
+        
+    }
+
+    removePlayer(player : Player) : void {
+        
+    }
+}
