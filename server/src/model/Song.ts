2cf028fab397e6bc71bbc4582c0874ac133c4e72
Amanda Papacosta <amanda.dyrell.papacosta@gmail.com>
2023-01-27 15:19:20 +0100

    changed song to interface instead

--- server/src/model/Song.ts
+++ server/src/model/Song.ts
@@ -1,13 +1,7 @@
-export class Song {
+export interface Song {
     name: string;
     album: string;
     artist: string;
     albumCover: string;
 
-    constructor(name: string, album: string, artist: string, albumCover: string) {
-        this.name = name;
-        this.album = album;
-        this.artist = artist;
-        this.albumCover = albumCover;
-    }
 }
