a075e58871a53c6cd2da8717608af4e7b6750b85
hampusrs <hampusrhedinstam@gmail.com>
2023-01-31 16:06:49 +0100

    Add gameRouter

---
+++ server/src/router/game.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import { startNewGame, addPlayer } from "../service/Game";
+//POST to /game with body { "action" : "start" }Responds with { currentSong : ..., currentPlayer : ...}, error response if game has already started.
+//POST to /game with body { "action" : "next song" }Responds with { currentSong: ..., currentPlayers: ...}, error response if game has not started.
+//POST to /game with body { "action" : "end game"}Responds with 200 empty body if game has started, error response if game has not started.
+export const gameRouter = express.Router();
+
+gameRouter.post("/game", (req, res) => {
+    const gameID = req.body.gameID;
+    const players = req.body.players;
+    const songs = req.body.songs;
+
+    const newGame = startNewGame(gameID, players, songs);
+    res.status(200).send(newGame);
+})
+
+gameRouter.post("/game/{gameID}/players", (req, res) => {
+    const gameID = req.body.gameID;
+    const name = req.body.name;
+    const topSongs = req.body.topSongs;
+
+    addPlayer(gameID, name, topSongs);
+    res.status(200);
+})
