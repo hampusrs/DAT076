38c0cf22ba4e31ded8020fdff64f632a0511d9e4
hampusrs <hampusrhedinstam@gmail.com>
2023-02-01 10:47:34 +0100

    Added comments

--- server/src/router/game.ts
+++ server/src/router/game.ts
@@ -5,11 +5,7 @@ import { startNewGame, addPlayer, getGames, addGame, getGame } from "../service/
 //POST to /game with body { "action" : "end game"} Responds with 200 empty body if game has started, error response if game has not started.
 export const gameRouter = express.Router();
 
-
-gameRouter.get("/game", (req, res) => { 
-    res.status(200).send(getGames())
-})
-
+// Create a game
 gameRouter.post("/game", (req, res) => {
     const gameID = req.body.gameID;
     const players = req.body.players;
@@ -19,11 +15,18 @@ gameRouter.post("/game", (req, res) => {
     res.status(200).send(newGame);
 })
 
+// Get all games
+gameRouter.get("/game", (req, res) => {
+    res.status(200).send(getGames())
+})
+
+// Get a game with a certain gameID
 gameRouter.get("/game/:gameID", (req, res) => {
     const gameID : number = parseInt(req.params.gameID, 10); 
     res.status(200).send(getGame(gameID));
 })
 
+// Add a player to a game with a certain gameID
 gameRouter.post("/game/:gameID/players", (req, res) => {
     const gameID : number = parseInt(req.params.gameID, 10);
     const name = req.body.name;
