e27a49ea41c870bee21ff51c4ee7fb4764a20522
Amanda Papacosta <amanda.dyrell.papacosta@gmail.com>
2023-02-01 10:12:05 +0100

    added getGame() method

--- server/src/router/game.ts
+++ server/src/router/game.ts
@@ -1,9 +1,14 @@
 import express from "express";
-import { startNewGame, addPlayer } from "../service/game";
+import { startNewGame, addPlayer, getGames, addGame, getGame } from "../service/game";
 //POST to /game with body { "action" : "start" } Responds with { currentSong : ..., currentPlayer : ...}, error response if game has already started.
 //POST to /game with body { "action" : "next song" } Responds with { currentSong: ..., currentPlayers: ...}, error response if game has not started.
 //POST to /game with body { "action" : "end game"} Responds with 200 empty body if game has started, error response if game has not started.
 export const gameRouter = express.Router();
+
+
+gameRouter.get("/game", (req, res) => { 
+    res.status(200).send(getGames())
+})
 
 gameRouter.post("/game", (req, res) => {
     const gameID = req.body.gameID;
@@ -14,6 +19,11 @@ gameRouter.post("/game", (req, res) => {
     res.status(200).send(newGame);
 })
 
+gameRouter.get("/game/:gameID", (req, res) => {
+    const gameID : number = parseInt(req.params.gameID, 10); 
+    res.status(200).send(getGame(gameID));
+})
+
 gameRouter.post("/game/{gameID}/players", (req, res) => {
     const gameID = req.body.gameID;
     const name = req.body.name;
@@ -21,5 +31,6 @@ gameRouter.post("/game/{gameID}/players", (req, res) => {
 
 
     addPlayer(gameID, name, topSongs);
-    res.status(200);
+    res.status(200).send(addPlayer);
 })
+
