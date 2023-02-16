8060b2a0c7660b60b3cb2cab8c8f1815894384a7
ArvidSve <73997840+ArvidSve@users.noreply.github.com>
2023-02-16 10:57:57 +0100

    adds showPlayers with button
    Currently displays all the players in the game with a press of the "Show all players" button. But currently doent work to retract the list agian with anouther button press.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,10 @@ import React, { useEffect, useState, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {SongItem} from './components/SongItem';
+import {PlayersView} from "./components/PlayersView"
 
 import axios from 'axios';
+import { Dropdown } from 'react-bootstrap';
 
 export interface Song {
   title: string;
@@ -29,13 +31,13 @@ export function App() {
   useEffect(() => {
     startGame();
   }, []);
-
+  
   async function getPlayers() {
     // request the games players, GET request and set players to the response
     const response = await axios.get<{players : Player[]}>("http://localhost:8080/game");
     setPlayers(response.data.players);
   }
-
+  
   async function startGame() {
     if (! startedGame.current){
       startedGame.current = true;
@@ -52,7 +54,12 @@ export function App() {
     setCurrentSong(response.data.currentSong);
     setCurrentPlayers(response.data.players);
   }
-  
+
+  // Creates a PlayerView component for given player.
+  function displayPlayer(player : Player) {
+    return <PlayersView pName={player.name}> </PlayersView>
+  }
+
   return (
     <div className="App">
       {(currentSong == null) 
@@ -60,11 +67,16 @@ export function App() {
       : <SongItem title={currentSong.title} artist={currentSong.artist} album={currentSong.album} albumCoverPath="./logo192.png" />}
       <label> Who has this song as one of their top song? </label>
       <button onClick={nextSong}>Next Song</button>
-      <aside> 
-        <button id="showPlayersButton">
-          Show All Players
-        </button>
-      </aside>
+      <div id="showAllPlayersDiv">
+        <button id="showPlayersButton" onClick={getPlayers}>Show all players</button>  
+        <div id="playersList">
+          
+          {/* Row below takes each player in players and applies displayPlayer to them */}
+          {players?.map(displayPlayer)}  
+        
+        </div>
+      </div>
+      
     </div>
   );
 }
