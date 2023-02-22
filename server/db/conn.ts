e12642e7a1e67d676b37adace703939c02fc569e
Amanda Papacosta <amanda.dyrell.papacosta@gmail.com>
2023-02-22 10:01:31 +0100

    Added connection to database

---
+++ server/db/conn.ts
@@ -0,0 +1,5 @@
+import { createConnection } from "mongoose";
+
+
+
+export const conn = createConnection("mongodb+srv://amandapapacosta:WebApplication2023@cluster0.exxnvpa.mongodb.net/?retryWrites=true&w=majority");
