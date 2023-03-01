3dd537b4e4f8a660899800cf03765261b628b21c
elinhagman <73999747+elinhagman@users.noreply.github.com>
2023-03-01 11:56:20 +0100

    Add spotify authorization and request for getting users top songs

--- server/src/router/game.ts
+++ server/src/router/game.ts
@@ -4,10 +4,14 @@ import { Player } from "../model/Player";
 import {makeGameService } from "../service/game";
 export const gameRouter = express.Router();
 const gameService = makeGameService();
-
-import querystring from 'query-string';
+import queryString from 'query-string';
 import dotenv from 'dotenv';
 dotenv.config();
+import axios from 'axios';
+
+const CLIENT_ID = process.env["CLIENT_ID"];
+const CLIENT_SECRET = process.env["CLIENT_SECRET"];
+const REDIRECT_URI = process.env["REDIRECT_URI"];
 
 gameRouter.get("/game", async (_, res) => {
   try {
@@ -45,25 +49,56 @@ gameRouter.post(
 );
 
 
-gameRouter.get('/login', (req, res) => {
+gameRouter.get('/login', (_, res) => {
   const state = generateRandomString(16);
   res.cookie('spotify_auth_state', state); 
 
   const scope = 'user-top-read';
 
-  const CLIENT_ID = process.env["CLIENT_ID"];
-  const CLIENT_SECRET = process.env["CLIENT_SECRET"];
-  const REDIRECT_URI = process.env["REDIRECT_URI"];
-
-  /*const queryParams = querystring.stringify({
+  const queryParams = queryString.stringify({
     client_id: CLIENT_ID,
     response_type: 'code',
     redirect_uri: REDIRECT_URI,
     state: state,
     scope: scope
-  });*/
-  res.redirect(`https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}`);
-  //res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
+  });
+  //res.redirect(`https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}`);
+  res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
+
+})
+
+gameRouter.get('/callback', (req,res) => {
+  const code = req.query["code"] || null;
+  
+  axios({
+    method: 'post',
+        url: 'https://accounts.spotify.com/api/token',
+        data: queryString.stringify({
+            grant_type: 'authorization_code',
+            code: code,
+            redirect_uri: REDIRECT_URI
+        }),
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            Authorization: `Basic ${new (Buffer as any).from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`
+        },
+  }).then(response => {
+    if (response.status === 200) {
+      // request spotify data
+      const {access_token, token_type} = response.data;
+
+      axios.get('https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=5', {
+        headers: {
+          Authorization: `${token_type} ${access_token}`
+        }
+      }).then(response => {
+        console.log(response.data);
+        res.send(`<pre>${JSON.stringify(response.data, null,2)}</pre>`) // response!
+      })
+    }
+  }).catch(error => { // response was not sucessful, no authorization code
+    res.send(error);
+  })
 
 })
 
