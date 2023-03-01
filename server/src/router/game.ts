8d5d4f17b428ef484b2564017c090be364d5a824
elinhagman <73999747+elinhagman@users.noreply.github.com>
2023-03-01 17:07:53 +0100

    Change from then and catch to asynd and await in /callback
    Also add api call to get user's user id and display name

--- server/src/router/game.ts
+++ server/src/router/game.ts
@@ -1,13 +1,13 @@
 import express, { Request, Response } from "express";
 import { Song } from "../model/Song";
 import { Player } from "../model/Player";
-import {makeGameService } from "../service/game";
+import { makeGameService } from "../service/game";
 export const gameRouter = express.Router();
 const gameService = makeGameService();
-import queryString from 'query-string';
-import dotenv from 'dotenv';
+import queryString from "query-string";
+import dotenv from "dotenv";
 dotenv.config();
-import axios from 'axios';
+import axios from "axios";
 
 const CLIENT_ID = process.env["CLIENT_ID"];
 const CLIENT_SECRET = process.env["CLIENT_SECRET"];
@@ -28,16 +28,19 @@ interface GameActionRequest extends Request {
 
 gameRouter.post(
   "/game",
-  async (req: GameActionRequest, res: Response<string | { currentSong : Song, players : Player[] }>) => {
+  async (
+    req: GameActionRequest,
+    res: Response<string | { currentSong: Song; players: Player[] }>
+  ) => {
     try {
       const action: string = req.body.action;
       if (action == "StartGame") {
-        if(gameService.startGame() == null) {
+        if (gameService.startGame() == null) {
           res.status(400).send(`Game has already been started`);
         }
         res.status(200).send(await gameService.startGame());
       } else if (action == "NextSong") {
-          res.status(200).send(await gameService.nextSong());
+        res.status(200).send(await gameService.nextSong());
       } else {
         res.status(400).send(`The action ${action} is not defined`);
       }
@@ -48,73 +51,108 @@ gameRouter.post(
   }
 );
 
-
-gameRouter.get('/login', (_, res) => {
+gameRouter.get("/login", (_, res) => {
   const state = generateRandomString(16);
-  res.cookie('spotify_auth_state', state); 
+  res.cookie("spotify_auth_state", state);
 
-  const scope = 'user-top-read';
+  const scope = "user-top-read user-read-email";
 
   const queryParams = queryString.stringify({
     client_id: CLIENT_ID,
-    response_type: 'code',
+    response_type: "code",
     redirect_uri: REDIRECT_URI,
     state: state,
-    scope: scope
+    scope: scope,
   });
-  //res.redirect(`https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}`);
   res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
+});
 
-})
-
-gameRouter.get('/callback', (req,res) => {
+// TODO: Better response handling
+gameRouter.get("/callback", async (req, res) => {
   const code = req.query["code"] || null;
-  
-  axios({
-    method: 'post',
-        url: 'https://accounts.spotify.com/api/token',
-        data: queryString.stringify({
-            grant_type: 'authorization_code',
-            code: code,
-            redirect_uri: REDIRECT_URI
-        }),
+
+  try {
+    const authResponse = await axios({
+      method: "post",
+      url: "https://accounts.spotify.com/api/token",
+      data: queryString.stringify({
+        grant_type: "authorization_code",
+        code: code,
+        redirect_uri: REDIRECT_URI,
+      }),
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+        Authorization: `Basic ${new (Buffer as any).from(
+          `${CLIENT_ID}:${CLIENT_SECRET}`
+        ).toString("base64")}`,
+      },
+    });
+
+    if (authResponse.status === 200) {
+      // access token has been granted
+      const { access_token, token_type } = authResponse.data;
+
+      // Get user's info
+      const userInfoResponse = await axios.get("https://api.spotify.com/v1/me", {
         headers: {
-            'content-type': 'application/x-www-form-urlencoded',
-            Authorization: `Basic ${new (Buffer as any).from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`
+          Authorization: `${token_type} ${access_token}`,
         },
-  }).then(response => {
-    if (response.status === 200) {
-      // request spotify data
-      const {access_token, token_type} = response.data;
+      }); 
+      // 401 (bad/expired token), 403 (bad oAuth req), 429
 
-      axios.get('https://api.spotify.com/v1/me/top/tracks?limit=20', {
-        headers: {
-          Authorization: `${token_type} ${access_token}`
+      // Get user's top tracks
+      const topTracksResponse = await axios.get(
+        "https://api.spotify.com/v1/me/top/tracks?limit=20",
+        {
+          headers: {
+            Authorization: `${token_type} ${access_token}`,
+          },
         }
-      }).then(response => {   // response with players songs
-        const topSongs : Song[] = [];
-        const tracks = response.data;
-        tracks.items.map((track : any) => {
-          //const albumCover : string = track.album.images[0];
-          const song : Song = {id : track.id, title : track.name, album : track.album.name, artist : track.artists[0].name}
-          topSongs.push(song);
-        })
-        console.log(topSongs)
-        //gameService.addPlayer()
-        res.send(`<pre>${JSON.stringify(response.data, null,2)}</pre>`) 
-      })
+      );
+      
+      // The fetched data needed to create a player object
+      const userinfo = userInfoResponse.data;
+      const tracks = topTracksResponse.data;
+
+      // Get the revelant data from the responses
+      // user name
+      const username : string = userinfo.id;
+      const display_name : string = userinfo.display_name;
+      let playerName : string;
+      if (display_name == null) { // if display name is not found
+        playerName = username;   // set player's name to only username
+      } else {
+        playerName = `${display_name} (${username})`;
+      }
+      
+      // top songs
+      const topSongs: Song[] = [];
+      tracks.items.map((track: any) => {
+        //const albumCover : string = track.album.images[0];
+        const song: Song = {
+          id: track.id,
+          title: track.name,
+          album: track.album.name,
+          artist: track.artists[0].name,
+        };
+        topSongs.push(song);
+      });
+
+      const newPlayer : Player = {name : playerName, topSongs : topSongs}
+      //gameService.addPlayer(player_name, topSongs) // TODO: fix when addPlayer has been implemented in gameService
+      res.send(JSON.stringify(newPlayer,null,2));
     }
-  }).catch(error => { // response was not sucessful, no authorization code
+  } catch (error) {
     res.send(error);
-  })
+  }
+});
 
-})
-
-const generateRandomString = (length : number) => {
-  let text = '';
-  const possible = 'ABDDEFGHIJKLMNOPQRSTUVXYZabcdefghiijklmnopqrstuvxyz0123456789';
+const generateRandomString = (length: number) => {
+  let text = "";
+  const possible =
+    "ABDDEFGHIJKLMNOPQRSTUVXYZabcdefghiijklmnopqrstuvxyz0123456789";
   for (let i = 0; i < length; i++) {
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
   return text;
-}
+};
