c15f0365d87f17255490c14417c370e67acf502c
ArvidSve <73997840+ArvidSve@users.noreply.github.com>
2023-03-06 12:45:47 +0100

    Adds tests and consisten error handling in service.

--- server/src/service/ServicesTests.test.ts
+++ server/src/service/ServicesTests.test.ts
@@ -1,5 +1,104 @@
 import { makeGameService } from "./game";
+import { Song } from "../model/Song";
+import { Player } from "../model/Player";
 
+/** Create mock-data, bob and jane */
+const song1: Song = {
+  id: 1,
+  title: "Firework",
+  album: "Teenage Dream",
+  artist: "Katy Perry",
+};
+const song2: Song = {
+  id: 2,
+  title: "Baby",
+  album: "My Worlds",
+  artist: "Justin Bieber",
+};
+const song3: Song = {
+  id: 3,
+  title: "Levels",
+  album: "True",
+  artist: "Avicii",
+};
+
+const player1: Player = { name: "Bob", topSongs: [song1, song2] };
+const player2: Player = { name: "Jane", topSongs: [song2, song3] };
+// const players: Player[] = [player1, player2];
+
+test("Test that addPlayer adds a player to currentPlayers", () => {
+  
+  const gameService = makeGameService();
+  // add bob to game
+  gameService.addPlayer(player1.name, [song1, song2]);
+  // check that currentPlayers contains bob
+  const numberMatches = gameService.allPlayers.filter(player => player.name == player1.name)
+  expect(numberMatches.length).toBeGreaterThanOrEqual(1); 
+});
+
+
+test("Test that startGame returns undefined when game has already started", async () => {
+  const gameService = makeGameService();
+  // start the game for the first time
+  await gameService.addPlayer(player1.name, player1.topSongs);
+  await gameService.addPlayer(player2.name, player2.topSongs);
+  expect(await gameService.startGame()).not.toBeUndefined;
+  // starts the game for a second time, should return undefined.
+  expect(await gameService.startGame()).toBeUndefined;
+});
+
+
+test("Check that startGame sets a currentSong", async () => {
+  const gameService = makeGameService();
+  // starts a game
+  gameService.addPlayer(player1.name, player1.topSongs);
+  gameService.addPlayer(player2.name, player2.topSongs);
+  
+  await gameService.startGame();
+
+  expect(gameService.currentSong).not.toBeUndefined;
+});
+
+
+test("Checks that isAlreadyStarted returns true if game is started otherwise not.", async () => {
+    const gameService = makeGameService();
+    gameService.addPlayer(player1.name, player1.topSongs);
+    gameService.addPlayer(player2.name, player2.topSongs);
+    expect((await gameService.isAlreadyStarted()).gameHasStarted).toBeFalsy;
+    gameService.startGame();
+    expect((await gameService.isAlreadyStarted()).gameHasStarted).toBeTruthy;
+  });
+
+  
+test("Check that startGame returns the currentSong and accurate list of players that has that song as top song", async () => {
+    const gameService = makeGameService();
+    gameService.addPlayer(player1.name, player1.topSongs);
+    gameService.addPlayer(player2.name, player2.topSongs);
+    const result = await gameService.startGame();
+    expect(result).not.toBeUndefined;
+    if (result == null) {
+        return;
+    }
+    const song = result.currentSong;
+    const playersWithSong = result.players;
+    //Checks so that the players returned by the game are the same as the players returned by function "findPlayersWithSong"
+    expect(playersWithSong.length).toEqual((await gameService.findPlayersWithSong(song)).length); 
+});
+
+test("Checks if findPlayersWithSong gives the correct players with that top song",async () => {
+
+})
+
+test("Test so adding one player doesn't return undefined but adding the same player again should",async () => {
+    const gameService = makeGameService();
+    expect(await gameService.addPlayer(player1.name, player1.topSongs)).not.toEqual(undefined);
+    expect(await gameService.addPlayer(player1.name, player1.topSongs)).toEqual(undefined);
+})
+
+test("Check that nextSong doesn't give the same song again.", () => {
+    // nextSong, Checks that the song given by the method isn't the same. 
+  });
+/*
 test("Test that the array of current players in the game have length 2", async () => {
     const gameService = makeGameService();
     const players = (await gameService.getPlayers()).players;
@@ -7,10 +106,10 @@ test("Test that the array of current players in the game have length 2", async (
 });
 
 test("Test if the first player is named Bob", async () => {
-    /*const gameService = makeGameService();
-    const players = gameService.getPlayers();
-    const name : string = (await players)[0].name; 
-    expect(name).toEqual("Bob");*/
+    // const gameService = makeGameService();
+    // const players = gameService.getPlayers();
+    // const name : string = (await players)[0].name; 
+    // expect(name).toEqual("Bob");
 });
 
 test("Check if the list of songs does not contain duplicates", async () => { 
@@ -46,3 +145,4 @@ test("Find player that has currentSong as top song", async () => {
 });
 
 
+*/
--- server/src/service/game.ts
+++ server/src/service/game.ts
@@ -4,9 +4,16 @@ import { Player } from "../model/Player";
 //const gameID: number = 123;
 
 interface IGameService {
+  // returns all the players currently in the game.
   getPlayers(): Promise<{ players: Player[] }>;
-  startGame(): Promise<{ currentSong: Song, players: Player[] } | undefined>; // returns undefined is game is already started.
+  // returns undefined is game is already started or there are no songs left.
+  startGame(): Promise<{ currentSong: Song, players: Player[] } | undefined>; 
+  // returns undefined if game is already started or there are no songs left.
   nextSong(): Promise<{ currentSong: Song, players: Player[] } | undefined>;
+  // returns undefined if player already exist.
+  addPlayer(username: string, topSongs: Song[]): Promise<Player | undefined>;
+  // returns object without currentSong if it is undefined.
+  isAlreadyStarted(): Promise<{ gameHasStarted: boolean, currentPlayers: Player[], currentSong?: Song }>;
 }
 
 class GameService implements IGameService {
@@ -22,13 +29,13 @@ class GameService implements IGameService {
 
   async startGame(): Promise<{ currentSong: Song, players: Player[] } | undefined> {
     if (this.currentSong == null) {
-      this.gameHasStarted = true;
       await this.setupSongs();
       this.currentSong = this.shuffledSongs.at(this.shuffledSongs.length - 1); //Pick first song in shuffledSongs.
       if (this.currentSong == null) {
         return undefined;
       }
-      return {currentSong : this.currentSong, players : this.allPlayers}
+      this.gameHasStarted = true;
+      return {currentSong : this.currentSong, players : await this.findPlayersWithSong(this.currentSong)}
     } else {
       return undefined;   //returns undefined if game is already started.
     }
@@ -37,9 +44,10 @@ class GameService implements IGameService {
   async addPlayer(username: string, topSongs : Song[]) {
     const p: Player = { name: username, topSongs: topSongs };
     if (this.allPlayers.filter((player => player.name == username)).length > 0) {
-      throw new Error(`Player with username: ${username} already exists`);
+      return undefined;
     }
     this.allPlayers.push(p);
+    return p;
   }
   
   private async setupSongs() : Promise<void> {
@@ -58,24 +66,31 @@ class GameService implements IGameService {
 
   async nextSong(): Promise<{ currentSong: Song; players: Player[]; } | undefined> {
     if (this.currentSong == null) {
-      throw new Error(`Game has not started yet`);
+      return undefined;
     } else {
       this.currentSong = this.shuffledSongs.pop();
       if (this.currentSong == null) {
         return undefined;
       }
-      return {currentSong : this.currentSong , players : this.allPlayers}
+      return {currentSong : this.currentSong , players : await this.findPlayersWithSong(this.currentSong)}
     }
   }
 
   async findSongs(): Promise<Song[]> {
     const uniqueSongs: Set<Song> = new Set(this.allPlayers.flatMap(player => player.topSongs));
+    if (uniqueSongs.size == 0) {
+      throw new Error(`The given players have no top songs.`);
+    }
     return Array.from(uniqueSongs);
   }
 
   // Returns all players that have the given song as one of their topsongs. 
   async findPlayersWithSong(currentSong: Song): Promise<Player[]> {
-    return this.allPlayers.filter(player => player.topSongs.includes(currentSong));
+    const playersWithSong : Player[] = this.allPlayers.filter(player => player.topSongs.includes(currentSong));
+    if(playersWithSong.length == 0) {
+      throw new Error(`No player have this song as one of their top songs.`);
+    }
+    return playersWithSong;
   }
 
   // Shuffles the given Song array.
