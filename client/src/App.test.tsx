61bcbceb3ab86a7ad80064ef7265d6e559382d90
hampusrs <hampusrhedinstam@gmail.com>
2023-03-13 16:13:47 +0100

    Create test: Login Button is rendered when App is

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 import {SongItem} from './components/SongItem';
+import axios, { AxiosStatic } from 'axios';
 
-test('Check that app renders next button', () => {
+/** Create the mocked version of Axios */
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<AxiosStatic>
+
+test('Check that app renders login button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Next Song/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const loginButton = screen.getByText(/Log in/);
+  expect(loginButton).toBeInTheDocument();
+
+  //TODO: Check that when the button is clicked, the user is redirected to http://localhost:8080/login
+  //fireEvent.click(loginButton);
+
+  //expect(window.location.href).toEqual('http://localhost:8080/login');
+  //expect(mockedAxios.get).toHaveBeenCalledWith("http://localhost:8080/login");
 });
 
+/*
 test('Check that song is rendered', () => {
   render(<SongItem title="FireWorks" artist = "Katy Perry" album="Teenage Dream" albumCoverURI="./logo.svg"></SongItem>);
   expect(screen.getByText(/Katy Perry/)).toBeInTheDocument();
@@ -18,5 +31,5 @@ test('Check that song is rendered', () => {
 test('Check that the start game request is only sent once', () => {
   
 });
-
+*/
 // How to test next song?
